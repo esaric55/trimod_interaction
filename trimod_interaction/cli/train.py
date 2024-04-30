e2f313e0a9b556e1e88648782ee4595f3a6a25ac
esaric55 <esaric00@fesb.hr>
2024-04-30 14:23:56 +0200

    Final commit

--- trimod_interaction/cli/train.py
+++ trimod_interaction/cli/train.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 from torchmetrics.classification import BinaryAccuracy, BinaryPrecision, BinaryRecall, BinaryF1Score
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 from tqdm import tqdm
 
 from torch.utils.data import DataLoader
@@ -17,10 +18,10 @@ import click
 def train(model, loader, optimizer, criterion, device):
     model.train()
     total_loss = 0
-    accuracy = BinaryAccuracy().cpu()
-    precision = BinaryPrecision().cpu()
-    recall = BinaryRecall().cpu()
-    f1 = BinaryF1Score().cpu()
+    accuracy = BinaryAccuracy().to(device)
+    precision = BinaryPrecision().to(device)
+    recall = BinaryRecall().to(device)
+    f1 = BinaryF1Score().to(device)
 
     for inputs, targets in tqdm(loader):
         inputs = inputs.to(device)
@@ -29,31 +30,35 @@ def train(model, loader, optimizer, criterion, device):
         optimizer.zero_grad()
         outputs= model(inputs)
         # TODO: calculate Accuracy/Precision/Recall/F1
-        predictions=outputs.argmax(axis=1)
-        acc = accuracy(predictions,targets)
+        preds=outputs.argmax(axis=1)
+        acc = accuracy(preds,targets)
 
         # Calculate metrics
-        prec = precision(predictions, targets)
-        rec = recall(predictions, targets)
-        f1_score = f1(predictions, targets)
+        prec = precision(preds, targets)
+        rec = recall(preds, targets)
+        f1_score = f1(preds, targets)
 
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
     # TODO: return Accuracy/Precision/Recall/F1
-        #avg_accuracy = acc.compute()
     # Compute average metrics
     avg_precision = precision.compute()
     avg_recall = recall.compute()
     avg_f1 = f1.compute()
-    avg_accuracy = accuracy(predictions, targets)  # Using torchmetrics accuracy
+    avg_accuracy = accuracy.compute()
     t_loss = total_loss/len(loader)
     return t_loss, avg_precision, avg_recall, avg_f1, avg_accuracy
 
 def test(model, loader, criterion, device):
     model.eval()
     total_loss = 0
+    accuracy = BinaryAccuracy().to(device)
+    precision = BinaryPrecision().to(device)
+    recall = BinaryRecall().to(device)
+    f1 = BinaryF1Score().to(device)
+
     with torch.no_grad():
         for inputs, targets in tqdm(loader):
             inputs = inputs.to(device)
@@ -61,11 +66,18 @@ def test(model, loader, criterion, device):
             targets = targets.to(device)
             outputs = model(inputs)
             # TODO: calculate Accuracy/Precision/Recall/F1
+            preds=outputs.argmax(axis=1)
+            acc = accuracy(preds,targets)
+     
+            prec = precision(preds, targets)
+            rec = recall(preds, targets)
+            f1_score = f1(preds, targets)
 
             loss = criterion(outputs, targets)
             total_loss += loss.item()
     # TODO: return Accuracy/Precision/Recall/F1
-    return total_loss / len(loader)
+            t_loss = total_loss/len(loader)
+    return t_loss, avg_precision, avg_recall, avg_f1, avg_accuracy
 
 @click.command()
 @click.option('--data-dir', default='../data/split', help='Directory path for data')
@@ -127,28 +139,25 @@ def main(data_dir, learning_rate, rgb, depth, thermal, num_epochs):
     precision_values = []
     recall_values = []
     f1_values = []
-    results = []
+    train_metrics = {'loss': [], 'accuracy': [], 'precision': [], 'recall': [], 'f1': []}
+    val_metrics = {'loss': []}
+
     # Training loop
     for epoch in range(num_epochs):
-        results = train(model, train_loader, optimizer, criterion, device)
         # TODO: store training metrics
-        train_loss, avg_precision, avg_recall, avg_f1, avg_accuracy = results
+        train_loss, avg_precision, avg_recall, avg_f1, avg_accuracy = train(model, train_loader, optimizer, criterion, device)
         
         # Store training metrics
         accuracy_values.append(avg_accuracy)
         precision_values.append(avg_precision)
         recall_values.append(avg_recall)
         f1_values.append(avg_f1)
+        train_metrics['loss'].append(train_loss)
+        train_metrics['accuracy'].append(avg_accuracy)
+        train_metrics['precision'].append(avg_precision)
+        train_metrics['recall'].append(avg_recall)
+        train_metrics['f1'].append(avg_f1)
 
-        #Plotting
-        metrics_names = ['Accuracy', 'Precision', 'Recall', 'F1']
-        metrics_values = [avg_accuracy, avg_precision, avg_recall, avg_f1]
-        plt.bar(metrics_names, metrics_values, color=['blue', 'green', 'orange', 'red'])
-        plt.ylabel('Metrics Value')
-        plt.title('Model Evaluation Metrics')
-        plt.show()
-
-    
         print(f"Epoch {epoch+1}, Train Loss: {train_loss}")
         print(f"Epoch {epoch+1}, Accuracy: {avg_accuracy}")
         print(f"Epoch {epoch+1}, Precision: {avg_precision}")
@@ -160,24 +169,57 @@ def main(data_dir, learning_rate, rgb, depth, thermal, num_epochs):
         val_loss = test(model, val_loader, criterion, device)
         # TODO: store validation metrics
         print(f"Epoch {epoch+1}, Validation Loss: {val_loss}")
+    
+    metrics_names = ['Accuracy', 'Precision', 'Recall', 'F1']
+    metrics_values = [avg_accuracy, avg_precision, avg_recall, avg_f1]
 
-        # TODO: Save model if validation loss is better
+    # TODO: Save model if validation loss is better
+    if epoch == 0 or val_loss < min(val_metrics['loss']):
+          torch.save(model.state_dict(), 'best_model.pth')
+
+    #Converting tensor to NumPy array
+    train_metrics_numpy = {key: np.array(value) for key, value in train_metrics.items()}
+    val_metrics_numpy = {key: np.array(value) for key, value in val_metrics.items()}
+
+    # Convert NumPy arrays to JSON-serializable format
+    train_metrics_json = {key: value.tolist() for key, value in train_metrics_numpy.items()}
+    val_metrics_json = {key: value.tolist() for key, value in val_metrics_numpy.items()}
+    
     # Save metrics to JSON file
     metrics_dict = {
-            'accuracy': accuracy_values,
-            'precision': precision_values,
-            'recall': recall_values,
-            'f1': f1_values
+    'train': train_metrics_json,
+    'validation': val_metrics_json
     }
     
     with open('metrics.json', 'w') as f:
             json.dump(metrics_dict, f)
+   
+    plt.bar(metrics_names, metrics_values, color=['blue', 'green', 'orange', 'red'])
+    plt.ylabel('Metrics Value')
+    plt.title('Model Evaluation Metrics')
+    plt.show()
 
     # Test loop
     # TODO: load best model
-    test_loss = test(model, test_loader, criterion, device)
+    model.load_state_dict(torch.load('best_model.pth'))
     # TODO: store validation metrics
+    test_loss, avg_precision, avg_recall, avg_f1, avg_accuracy = test(model, test_loader, criterion, device)
     print(f"Test Loss: {test_loss}")
+    print(f"Precision: {avg_precision}")
+    print(f"Recall: {avg_recall}")
+    print(f"F1 Score: {avg_f1}")
+    print(f"Accuracy: {avg_accuracy}")
+
+    metrics__test_dict = {
+    'test loss': test_loss,
+    'precision': avg_precision,
+    'recall' : avg_recall,
+    'F1 Score' : avg_f1,
+    'Accuracy' : avg_accuracy
+    }
+    
+    with open('test_metrics.json', 'w') as f:
+            json.dump(metrics_test_dict, f)
 
 if __name__ == "__main__":
     main()
